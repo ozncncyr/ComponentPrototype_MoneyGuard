afc6240f2e808028da363a668d669c2f0cd929ad
Ozan Can CUYAR <ozncncyr@outlook.com>
2025-10-14 16:49:34 +0300

    feat: add selector for total amount of transactions

--- src/redux/transactions/selectors.js
+++ src/redux/transactions/selectors.js
@@ -1,3 +1,8 @@
 export const selectTransactions = (state) => state.transactions.items;
 export const selectLoading = (state) => state.transactions.loading;
 export const selectError = (state) => state.transactions.error;
+export const selectTotalAmount = (state) => {
+  return state.transactions.items.reduce((total, transaction) => {
+    return total + transaction.amount;
+  }, 0);
+};
